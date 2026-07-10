159abe8a2de1fcbe74312b3e9deee4f32f60c0c1
MerryBear17 <meribermudo@gmail.com>
2026-07-11 02:42:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,15 @@ beginButton.addEventListener("click", () => {
     intro.style.display = "none";
     letterSection.style.display = "flex";
 
-    if (music) {
-        music.play().catch(() => {});
+   if (music) {
+    music.volume = 0.5; // 50% volume
+    music.play()
+        .then(() => console.log("Music started!"))
+        .catch(err => {
+            console.error("Music failed to play:", err);
+            alert("Music failed to play: " + err.message);
+        });
+}
     }
 
     typeLetter();
